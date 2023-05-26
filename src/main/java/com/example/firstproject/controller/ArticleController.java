9be2e9c8ad2e358267d035d647f4b9a080c24982
mink0 <carone26@naver.com>
2023-05-26 11:10:54 +0900

    Initial Commit

---
+++ src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -0,0 +1,38 @@
+package com.example.firstproject.controller;
+
+import com.example.firstproject.dto.ArticleForm;
+import com.example.firstproject.entity.Article;
+import com.example.firstproject.repository.ArticleRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+@Slf4j
+public class ArticleController {
+
+    @Autowired // spring boot가 미리 생성해놓은 객체를 가져다가 자동 연결
+    private ArticleRepository articleRepository;
+
+    @GetMapping("/articles/new")
+    public String newArticleForm(){
+        return "articles/new";
+    }
+
+    @PostMapping("/articles/create")
+    public String createArticle(ArticleForm form){
+        log.info(form.toString());
+
+        // 1. Dto를 entity로 변환
+        Article article = form.toEntity();
+        log.info(article.toString());
+
+        // 2. Repository에게 entity를 DB안에 저장하도록
+        Article saved = articleRepository.save(article);
+        log.info(saved.toString());
+
+        return "";
+    }
+}
---
+++ src/main/java/com/example/firstproject/entity/Article.java
@@ -0,0 +1,24 @@
+package com.example.firstproject.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity // DB가 해당 객체를 인식 가능
+@AllArgsConstructor
+@ToString
+public class Article {
+
+    @Id  // 대표값 지정
+    @GeneratedValue  // 자동 생성 annotation
+    private Long id;
+    @Column
+    private String title;
+    @Column
+    private String content;
+
+}
