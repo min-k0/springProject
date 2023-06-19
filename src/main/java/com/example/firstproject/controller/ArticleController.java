20e6708d462a7c695db8c4c66b0d72e78499408b
mink0 <carone26@naver.com>
2023-06-19 17:14:10 +0900

    15. 데이터 수정하기

--- src/main/java/com/example/firstproject/controller/ArticleController.java
+++ src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -77,4 +77,25 @@ public class ArticleController {
         // 뷰 페이지 설정
         return "/articles/edit";
     }
+
+    @PostMapping("/articles/update")
+    public String update(ArticleForm form){
+        log.info(form.toString());
+
+        // 1. DTO를 entity로 변환
+        Article articleEntity = form.toEntity();
+        log.info(articleEntity.toString());
+
+        // 2. entity를 DB로 저장
+        // 2-1. DB에서 기존 데이터를 가져온다
+        Article target = articleRepository.findById(articleEntity.getId()).orElse(null);
+
+        // 2-2. 기존 데이터의 값을 갱신
+        if(target != null){
+            articleRepository.save(articleEntity);
+        }
+
+        // 3. 수정 결과 페이지로 redirect
+        return "redirect:/articles/" + articleEntity.getId();
+    }
 }
